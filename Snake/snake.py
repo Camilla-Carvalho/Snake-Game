aee79e5fafe448ef1cf3ff4a9216dc1f1faaa0fe
Crowmilla <camilla.workd2@gmail.com>
2020-11-12 14:00:25 -0300

    Update snake.py
    Desenvolvimento da cobra e da maçã
    Tratamento de disperção pela tela (posições)
    Atualização de libs

--- Snake/snake.py
+++ Snake/snake.py
@@ -1,6 +1,7 @@
 import pygame
+import random
 from pygame.locals import *
- 
+
 UP = 0
 LEFT = 1
 RIGHT = 2
@@ -10,17 +11,28 @@ pygame.init()
 screen = pygame.display.set_mode((600, 600))  # Objeto de tela
 pygame.display.set_caption('Snake')
 
-snake = [(200, 200), (210, 200), (220, 200)] #lista de segmentos // Cada segmento é representado por uma tupla 
+# lista de segmentos // Cada segmento é representado por uma tupla
+snake = [(200, 200), (210, 200), (220, 200)]
+snake_skin = pygame.pygame.Surface(10, 10)  # Snake Design
+snake_skin.pygame.Surface.fill(255, 255, 255)
+
+# 590 último espaço da tela
+apple_pos = (random.randint(0, 590), random.randint)
+apple = pygame.pygame.Surface.Surface(10, 10)  # Design Maçã
+apple.pygame.Surface.fill(255, 0, 0)
+
 my_direction = LEFT
 
 while True:
 
-    for event in pygame.event.get():
+    for event in pygame.event.get():  # Tratamento de escolhas
         if event.type == quit:
             pygame.quit()
-    screen = fill(0, 0, 0)
 
-    for pos in s
+    screen.fill(0, 0, 0)
+    screen.pygame.Surface.blit(apple, apple_pos)
+
+    for pos in snake:
+        screen.pygame.Surface.blit(snake_skin, pos)
 
 pygame.display.update()
-
